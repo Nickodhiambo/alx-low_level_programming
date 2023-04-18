db573b9b77a754d417e9fe34665c79eacae70f97
Nick <nodhiambo01@gmail.com>
2023-04-18 01:28:57 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - A dog's attributes
+ * @name: Name of dog
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description- Three attributes of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif

5dab8322d072942ea504e31295aab2d84df89728
Nick <nodhiambo01@gmail.com>
2023-10-11 06:33:54 -0700

    Binary Search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Searches for an integer value in an array
+ * of integers using binary search
+ * @array: The array to be searched
+ * @size: The array size
+ * @value: The value to search
+ * Return: The index of searched value or -1 if not found
+ *
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+	return (-1);
+}
